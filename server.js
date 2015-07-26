8d6f055f545c42f26caf530accb1ac5b68fddca6
James Balchunas <jbalchun@gmail.com>
2015-07-25 20:21:07 -0400

    go

--- server.js
+++ server.js
@@ -18,7 +18,7 @@
      var express = require('express'),
          app = express();
      winstonExpress(app, winston);
-     //app.use(express.static('www'));
+     app.use(express.static('www'));
      app.set('port', process.env.PORT || 5000);
      app.listen(app.get('port'), function () {
          console.log('Express server listening on port ' + app.get('port'));
